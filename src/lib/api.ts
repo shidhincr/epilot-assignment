cf6e91b44e5800e17beb2ba4b26114ce0e209299
Shidhin <shidhincr@gmail.com>
2023-11-17 19:33:46 +0530

    fix(api): add the gh token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,17 @@
 import ky from "ky";
 // import dummyReposJSON from "./dummy-repos.json";
 
+const headers: Record<string, any> = {};
+if (import.meta.env.VITE_GH_TOKEN) {
+  headers["Authorization"] = `token ${import.meta.env.VITE_GH_TOKEN}`;
+  console.log({ headers });
+}
+
 const api = ky.create({
   prefixUrl: "https://api.github.com/",
+  headers: {
+    ...headers,
+  },
 });
 
 const getUsers = async ({ query }) => {
