614ce9e8db06b7c2baab1a6a6948f86e59c38f3b
Shidhin <shidhincr@gmail.com>
2023-11-18 17:59:05 +0530

    fix(user): Grid skeleton UI issues

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -4,5 +4,9 @@ type LineProps = {
 };
 
 export const Line = ({ width, height }: LineProps) => {
-  return <div className={`bg-gray-200 w-[${width}px] h-${height}`}>&nbsp;</div>;
+  return (
+    <div className={`bg-gray-200 w-[${width}px] h-3 max-h-${height}`}>
+      &nbsp;
+    </div>
+  );
 };
